057b2cba396c06e356e84ee43d7a311a6fe8ead7
Anmol Verma <anmolverma102002@gmail.com>
2023-07-20 23:29:58 +0530

    feat: :eyes: implemented Review Service

--- src/main/java/dev/anmol/movies/ReviewService.java
+++ src/main/java/dev/anmol/movies/ReviewService.java
@@ -1,7 +1,24 @@
 package dev.anmol.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+
 public class ReviewService {
-    public Review createReview(){
-        
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
     }
 }
