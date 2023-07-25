2467814417165e27fb1911adb8d07d5184e4167a
Anmol Verma <anmolverma102002@gmail.com>
2023-07-26 01:27:11 +0530

    feat: :sparkles: added hero, home components in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import api from './api/axiosConfig'
 import axios from 'axios';
+import Layout from './components/Layout';
+import {Routes,Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -13,7 +15,6 @@ function App() {
     try
     {
       const response = await axios.get("http://localhost:8080/api/v1/movies");
-      console.log(response.data);
       setMovies(response.data);
     } 
     catch(err)
@@ -29,7 +30,11 @@ function App() {
 
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies}/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
